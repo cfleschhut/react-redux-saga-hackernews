02521fcbe8fe59608b17f6e13e57529476c454d1
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-22 12:39:11 +0100

    Add Redux, add example reducer, dispatch example actions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,33 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import reducer from './reducers';
+import { createStore } from 'redux';
+
+const store = createStore(reducer, []);
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: 'TODO_ADD',
+  todo: {
+    id: '0',
+    name: 'learn redux',
+    completed: false,
+  },
+});
+
+store.dispatch({
+  type: 'TODO_TOGGLE',
+  todo: {
+    id: '0',
+  },
+});
+
+unsubscribe();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

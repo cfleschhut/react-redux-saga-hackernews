9a5730a708449ac65b18bf8a64d9095611860d8d
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-22 17:58:51 +0100

    Add story reducer + initial state

--- src/index.js
+++ src/index.js
@@ -1,57 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-
-import reducer from './reducers';
-import { createStore } from 'redux';
-
-const store = createStore(reducer, []);
-
-const unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch({
-  type: 'TODO_ADD',
-  todo: {
-    id: '0',
-    name: 'learn redux',
-    completed: false,
-  },
-});
-
-store.dispatch({
-  type: 'TODO_TOGGLE',
-  todo: {
-    id: '0',
-  },
-});
-
-unsubscribe();
-
-const stories = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+import store from './store';
+import './index.css';
 
 ReactDOM.render(
-  <App stories={stories} onArchive={id => console.log(id)} />,
+  <App stories={store.getState()} onArchive={id => console.log(id)} />,
   document.getElementById('root'),
 );
 

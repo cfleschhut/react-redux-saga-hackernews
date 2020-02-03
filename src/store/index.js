cd3b3eb65ea8b834aa83effeebee015c9869de08
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-02-03 21:21:34 +0100

    Add Redux DevTools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
@@ -7,7 +7,8 @@ import rootSaga from '../sagas';
 const sagaMiddleware = createSagaMiddleware();
 const logger = createLogger();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware, logger)));
 
 sagaMiddleware.run(rootSaga);
 
